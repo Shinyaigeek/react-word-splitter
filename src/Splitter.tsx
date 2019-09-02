077eeaff2b9cb70f9ea62621acd3e138753cd924
svpcadmin <PhilisPaxil@gmail.com>
2019-09-02 18:36:49 +0900

    [fix]functional component to class compoentn

--- src/Splitter.tsx
+++ src/Splitter.tsx
@@ -16,12 +16,12 @@ function mergeWord(
 	return span;
 }
 
-function getIndex(index:number,option:"number" | string[]){
-    if(option === "number"){
-        return String(index + 1)
-    }else{
-        return option[index % option.length]
-    }
+function getIndex(index: number, option: "number" | string[]) {
+	if (option === "number") {
+		return String(index + 1);
+	} else {
+		return option[index % option.length];
+	}
 }
 
 function splitByOption(obj: string, rule: "words" | "chars") {
@@ -34,17 +34,34 @@ function splitByOption(obj: string, rule: "words" | "chars") {
 	}
 }
 
-function getEachNode(props:SplitterProps){
-    const splittedValue = splitByOption(props.words,props.rule)
-    const nodes = splittedValue.map((obj:string,index:number) => mergeWord(obj,props.eachClass,props.eachIdWithIndex,getIndex(index,props.indexOption)))
-    return nodes
+function getEachNode(props: SplitterProps) {
+	const splittedValue = splitByOption(props.words, props.rule);
+	const nodes = splittedValue.map((obj: string, index: number) =>
+		mergeWord(
+			obj,
+			props.eachClass,
+			props.eachIdWithIndex,
+			getIndex(index, props.indexOption)
+		)
+	);
+	return nodes;
 }
 
-export default function Splitter(props: SplitterProps) {
-    const nodes = getEachNode(props)
-	return (
-		<React.Fragment>
-			{nodes}
-		</React.Fragment>
-	);
+// export default function Splitter(props: SplitterProps):React.FunctionComponent {
+//     const nodes = getEachNode(props)
+// 	return (
+// 		<React.Fragment>
+// 			{nodes}
+// 		</React.Fragment>
+// 	);
+// }
+
+export default class Splitter extends React.Component<SplitterProps, {}> {
+	constructor(props: SplitterProps) {
+		super(props);
+	}
+	render() {
+		const nodes = getEachNode(this.props);
+		return <React.Fragment>{nodes}</React.Fragment>;
+	}
 }
