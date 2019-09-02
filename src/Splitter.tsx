a69b1fd78f4c5e25bb6368b66e00fd511197f305
svpcadmin <PhilisPaxil@gmail.com>
2019-09-02 17:55:26 +0900

    [add]function for arrange

---
+++ src/Splitter.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import { SplitterProps } from "./@types/react-word-splitter";
+
+function mergeWord(
+	obj: string,
+	eachClass: string,
+	eachIdWithIndex: string,
+	index: string
+) {
+	const span = document.createElement("span");
+	const id = eachIdWithIndex + "__" + index;
+	span.setAttribute("class", eachClass);
+	span.setAttribute("id", id);
+	span.textContent = obj;
+	return span;
+}
+
+function getIndex(index:number,option:"number" | string[]){
+    if(option === "number"){
+        return String(index + 1)
+    }else{
+        return option[index % option.length]
+    }
+}
+
+function splitByOption(obj: string, rule: "words" | "chars") {
+	if (rule === "words") {
+		const splittedValue = obj.split("");
+		return splittedValue;
+	} else {
+		const splittedValue = obj.split(" ");
+		return splittedValue;
+	}
+}
+
+function getEachNode(props:SplitterProps){
+    const splittedValue = splitByOption(props.words,props.rule)
+    const nodes = splittedValue.map((obj:string,index:number) => mergeWord(obj,props.eachClass,props.eachIdWithIndex,getIndex(index,props.indexOption)))
+    return nodes
+}
+
+export default function Splitter(props: SplitterProps) {
+    const nodes = getEachNode(props)
+	return (
+		<React.Fragment>
+			{nodes}
+		</React.Fragment>
+	);
+}
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     mode: 'development',
 
-    entry: './src/index.tsx',
+    entry: './src/index.ts',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
